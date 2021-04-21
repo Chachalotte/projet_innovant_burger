61df44ec66f600083aaaa9462d8770927ce23126
Fabien Kindt <fabien.kindt@gmail.com>
2021-04-21 09:27:24 +0200

    Omg ajout des fichiers d

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
